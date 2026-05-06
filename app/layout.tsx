c63f994fdf244f3c1a65ba82f8ba40d65f3e968e
smart-star <folux31@gmail.com>
2026-05-06 02:24:44 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
+      lang='en'
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+      <body className='min-h-full flex flex-col'>
+        <ClerkProvider>{children}</ClerkProvider>
+      </body>
     </html>
   );
 }
